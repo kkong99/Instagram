47f153b74d27770d0b35c7affd2fe98d6ff1d0a2
Kimora Kong <kekong99@fb.com>
2018-07-09 15:09:24 -0700

    Initial Commit

---
+++ Instagram/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instagram
+//
+//  Created by Kimora Kong on 7/9/18.
+//  Copyright © 2018 Kimora Kong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
